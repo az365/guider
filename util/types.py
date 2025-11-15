9f94b14c8d06f5bcba83e3ded13c711f1652d2e8
az365 <artemcarr@yandex.ru>
2025-11-16 01:11:39 +0300

    Unit: translate() method added

--- util/types.py
+++ util/types.py
@@ -1,6 +1,12 @@
 from typing import Union, Type, Callable
 
-Class = Union[Type, Callable]
+Numeric = Union[int, float]
+NUMERIC = int, float
+
+Primitives = Union[bool, int, float, str]
+PRIMITIVES = bool, int, float, str
+
 Array = Union[list, tuple]
 ARRAY_TYPES = list, tuple
-PRIMITIVES = bool, int, float, str
+
+Class = Union[Type, Callable]
--- visual/size.py
+++ visual/size.py
@@ -1,5 +1,9 @@
 from enum import Enum
+from typing import Optional
+from collections import OrderedDict
 
+from util.const import DEFAULT_FONT_SIZE, DEFAULT_FONT_PROPORTION
+from util.types import Numeric
 from util.functions import get_attr_str
 
 
@@ -8,9 +12,54 @@ class Unit(Enum):
     Char = 'ch'  # ширина символа 0
     Ephemeral = 'em'  # размер шрифта
 
+    @staticmethod
+    def translate(
+            x: Optional[Numeric],
+            src,  # from: Unit
+            dst,  # to: Unit
+            font_size: int = DEFAULT_FONT_SIZE,
+            font_proportion: float = DEFAULT_FONT_PROPORTION,
+    ) -> Optional[Numeric]:
+        if x is None:
+            return None
+        if src == dst:
+            return x
+        if not isinstance(src, Unit):
+            src = Unit(src)
+        if not isinstance(dst, Unit):
+            dst = Unit(dst)
+        x_dst = None
+        if dst == Unit.Pixel:
+            if src == Unit.Ephemeral:
+                x_dst = x * font_size
+            if src == Unit.Char:
+                x_dst = x * font_size * font_proportion
+        if dst == Unit.Ephemeral:
+            if src == Unit.Pixel:
+                x_dst = x / font_size
+            if src == Unit.Char:
+                x_dst = x / font_proportion
+        if dst == Unit.Char:
+            if src == Unit.Pixel:
+                x_dst = x / font_size / font_proportion
+            if src == Unit.Ephemeral:
+                x_dst = x / font_proportion
+        if x_dst is not None:
+            return x_dst
+        else:
+            raise NotImplementedError([src, dst])
+
+DEFAULT_UNIT = Unit.Pixel
 
 class Size:
-    def __init__(self, x, y, unit: Unit = Unit.Pixel, font_size: int = 16, font_proportion: float = 0.6):
+    def __init__(
+            self,
+            x,
+            y,
+            unit: Unit = DEFAULT_UNIT,
+            font_size: int = DEFAULT_FONT_SIZE,
+            font_proportion: float = DEFAULT_FONT_PROPORTION,
+    ):
         self._x = x
         self._y = y
         if not isinstance(unit, Unit):
@@ -27,35 +76,9 @@ class Size:
         return f'{self._y}{self.unit.value or ""}'
 
     def get_for_units(self, unit: Unit):
-        if unit == self.unit:
-            return self
-        else:
-            x, y = None, None
-        if unit == Unit.Pixel:
-            if self.unit == Unit.Ephemeral:
-                x = self._x * self.font_size
-                y = self._y * self.font_size
-            if self.unit == Unit.Char:
-                x = self._x * self.font_size * self.font_proportion
-                y = self._y * self.font_size * self.font_proportion
-        if unit == Unit.Ephemeral:
-            if self.unit == Unit.Pixel:
-                x = self._x / self.font_size
-                y = self._y / self.font_size
-            if self.unit == Unit.Char:
-                x = self._x / self.font_proportion
-                y = self._y / self.font_proportion
-        if unit == Unit.Char:
-            if self.unit == Unit.Pixel:
-                x = self._x / self.font_size / self.font_proportion
-                y = self._y / self.font_size / self.font_proportion
-            if self.unit == Unit.Ephemeral:
-                x = self._x / self.font_proportion
-                y = self._y / self.font_proportion
-        if x is not None and y is not None:
-            return Size(x, y, unit)
-        else:
-            raise NotImplementedError
+        x = self.x_size.get_for_units(unit)
+        y = self.y_size.get_for_units(unit)
+        return Size(x, y, unit=unit, **self._get_font_kwargs())
 
     def get_lines_count(self, round_factor: int = 1) -> float:
         y = self.get_for_units(Unit.Ephemeral)._y
@@ -64,6 +87,15 @@ class Size:
     def get_line_len(self, round_factor: int = 0) -> float:
         x = self.get_for_units(Unit.Char)._x
         return round(x, round_factor)
+
+    def _get_font_kwargs(self) -> OrderedDict:
+        return OrderedDict(font_size=self.font_size, font_proportion=self.font_proportion)
+
+    def _set_font_kwargs(self, font_kwargs: dict, skip_empty: bool = True):
+        if font_kwargs.get('font_size') or not skip_empty:
+            self.font_size = font_kwargs['font_size']
+        if font_kwargs.get('font_proportion') or not skip_empty:
+            self.font_proportion = font_kwargs['font_proportion']
 
     def __repr__(self):
         cls = self.__class__.__name__
