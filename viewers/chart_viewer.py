35f7686564dbc72b42c71328532dd338c9258643
az365 <artemcarr@yandex.ru>
2025-12-28 22:09:25 +0300

    ChartViewer refactoring: _get_bar_row() method extracted

--- viewers/chart_viewer.py
+++ viewers/chart_viewer.py
@@ -80,39 +80,66 @@ class BarChartViewer(SquareViewer):
             scale_x = bar_frame_size.x / max_value_rounded
             scale_x = int(scale_x.numeric)
         for k, v in obj.items():
-            hint = f'{k}: {v}'
-            if self.axis_width:
-                caption = str(v)
-                axis_mark = SquareView(
-                    data=k,
-                    tag=TagType.Div,
-                    size=mark_size,
-                    style=MARK_STYLE,
-                    hint=k,
-                )
-                bar_style.display = 'inline-block'
-                axis_mark.style.display = 'inline-block'
-            else:
-                caption = f'{k}: {v}'
-                axis_mark = None
-            bar_width = v * scale_x
-            bar_size = Size2d(bar_width, bar_frame_size.y)
-            bar = SquareView(
-                data=caption,
-                tag=TagType.Span,
-                size=bar_size,
-                style=bar_style,
-                hint=hint,
+            row = self._get_bar_row(
+                row_name=k,
+                value=v,
+                scale_x=scale_x,
+                row_frame_size=row_frame_size,
+                mark_size=mark_size,
+                bar_style=bar_style,
             )
-            if axis_mark:
-                row = SquareView(
-                    [axis_mark, bar],
-                    tag=None,
-                    size=row_frame_size,
-                    style=ROW_STYLE,
-                    hint=hint,
-                )
-            else:
-                row = bar
             bar_chart_view.data.append(row)
         return bar_chart_view
+
+    def _get_bar_row(
+            self,
+            row_name: str,
+            value: Union[float, dict],
+            scale_x: float,
+            row_frame_size: Size2d,
+            mark_size: Size2d,
+            bar_style: Style,
+    ) -> SquareView:
+        bar_frame_size = Size2d(row_frame_size.x - self.axis_width, row_frame_size.y)
+        if isinstance(value, (int, float)):
+            sum_value = value
+        elif isinstance(value, dict):
+            sum_value = sum(value.values())
+        else:
+            raise TypeError(value)
+        hint = f'{row_name}: {sum_value}'
+        if self.axis_width:
+            caption = str(sum_value)
+            axis_label = SquareView(
+                data=row_name,
+                tag=TagType.Div,
+                size=mark_size,
+                style=MARK_STYLE,
+                hint=row_name,
+            )
+            bar_style.display = 'inline-block'
+            axis_label.style.display = 'inline-block'
+        else:
+            caption = f'{row_name}: {sum_value}'
+            axis_label = None
+        bar_width = sum_value * scale_x
+        bar_size = Size2d(bar_width, bar_frame_size.y)
+        bar = SquareView(
+            data=caption,
+            tag=TagType.Span,
+            size=bar_size,
+            style=bar_style,
+            hint=hint,
+        )
+        if axis_label:
+            row = SquareView(
+                [axis_label, bar],
+                tag=None,
+                size=row_frame_size,
+                style=ROW_STYLE,
+                hint=hint,
+            )
+        else:
+            row = bar
+        assert isinstance(row, SquareView)
+        return row
